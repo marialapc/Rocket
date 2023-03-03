9469b1b68680336d238f7b789e89099391afae32
marialapc <marialopez.apc@gmail.com>
2023-03-03 11:29:17 +0100

    add text to the canvas

--- game.js
+++ game.js
@@ -4,6 +4,14 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame)
 
 function startGame(){
- 
+    // game.fillRect(0,0,100,100);
+    // game.clearRect(50,50,50,50);
+    // game.clearRect()
+    // game.clearRect(0,0,50,50)
+
+    game.font = '25px Verdana'
+    game.fillStyle = 'purple';
+    game.textAlign ='start';
+    game.fillText('Hi world', 25, 25)
     
 }
